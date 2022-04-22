ef14703c12e80246e491b1e8086a6ace8f744aa2
luminecs <wangxuke.china@gmail.com>
2022-04-22 15:10:53 +0800

    learn nextjs

---
+++ nextjs/from-react-to-nextjs/pages/index.jsx
@@ -0,0 +1,32 @@
+// index.html => index.js || index.jsx
+// move to pages folder
+// export default ...
+// add package scripts
+import { useState } from 'react'
+
+function Header ({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>
+}
+
+export default function HomePage (){
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
+
+  const [likes, setLikes] = useState(0)
+
+  function handleClick () {
+    setLikes(likes + 1)
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀"/>
+      <ul>
+        {names.map(name => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  )
+}
