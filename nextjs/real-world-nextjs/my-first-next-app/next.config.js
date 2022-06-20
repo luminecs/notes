f4debca37c401514d14052a8165f196deeb65233
luminecs <wangxuke.china@gmail.com>
2022-06-20 10:06:59 +0800

    nextjs

---
+++ nextjs/real-world-nextjs/my-first-next-app/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // webpack: (config, options) => {
+    // config.module.rules.push({
+      // test: /\.js/,
+      // use: [
+        // options.defaultLoaders.babel,
+        // This is just an example
+      // ]
+    // })
+  // }
+}
+
+module.exports = nextConfig
